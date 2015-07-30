1d8b778c0eb7360804e67ae99e03b18c2dbb4f87
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-07-29 23:51:19 -0400

    Add Author Model
    could make an AuthorMeta model I suppose, add a bio field or something
    like that.

---
+++ src/main/scala/info/ethanjoachimeldridge/model/Author.scala
@@ -0,0 +1,8 @@
+package info.ethanjoachimeldridge.model
+
+/** An Author of a Book 
+ * 
+ * @param id The id of the author
+ * @param name The authors name in their homeland's language
+ */
+case class Author(val id: Long, val name: String)
