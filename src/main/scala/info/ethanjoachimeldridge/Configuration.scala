2e4d58dd85435fe29cfcf73366fe9502ee07df48
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-07-29 22:34:25 -0400

    Add Application Entry Point and Config
    Add configuration class to hold configuration details and to prevent us
    litering the code with random text strings reference application.conf
    
    Add Boot to create the spray server and perform the bindings on the
    Actors that will constitute the service.

---
+++ src/main/scala/info/ethanjoachimeldridge/Configuration.scala
@@ -0,0 +1,15 @@
+package info.ethanjoachimeldridge
+
+import com.typesafe.config.ConfigFactory
+
+/** Simple configuration object to reduce hardcoded string usage in codebase */
+object Configuration {
+	private val config = ConfigFactory.load();
+
+	/** interface (localhost|ip addr|etc) to which the public http service will bind to */
+	val httpInterface = config.getString("bindings.http.interface")
+
+	/** port which the public http service will run on  */
+	val httpPort = config.getInt("bindings.http.port")
+
+}
