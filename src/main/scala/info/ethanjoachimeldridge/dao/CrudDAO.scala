0e92ce3f7ea9174b1af2ee2b09ca56bab559d770
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-08-02 23:52:02 -0400

    Update CrudDAO
    Making more generic

--- src/main/scala/info/ethanjoachimeldridge/dao/CrudDAO.scala
+++ src/main/scala/info/ethanjoachimeldridge/dao/CrudDAO.scala
@@ -1,44 +1,50 @@
 package info.ethanjoachimeldridge.dao
 
-import scala.concurrent._
+import scala.concurrent.{ExecutionContext, Future}
 
-/** Create. Read. Update. Remove trait for data access of models 
- * 
- * Each method takes an implicit ExecutionContext to run the future threads 
- * in. This is to support async access to datastores for a flexible data layer.
+/** Create. Read. Update. Delete. Trait for data access of models
+ *
+ * Each method takes an implicit ExecutionContext to run futures.
+ * This is to performing async database operations if neccesary.
+ * @tparam T The type of model which an implementing class will handle
  */
 trait CrudDAO[T] {
-	/** Create a model of type T in the underlying datastore
-	 * 
-	 * @param model The model which will be created in the datastore
-	 * @param ec Implicit execution context in which to run threads in
-	 * @return A Future containing an option holding the created object or None
-	 */
-	def create(model: T)(implicit ec: ExecutionContext) : Future[Option[T]]
-
-	/** Find a model by its ID.  
-	 * 
-	 * @param id The id of the model you would like to find in the datastore
-	 * @param ec Implicit execution context containing in which to run threads in
-	 * @return An option containing the found type T or None if unavailable or non-existent
-	 */
-	def findById(id: Long)(implicit ec: ExecutionContext) : Future[Option[T]]
-
-	/** Update a model to have the same values as the given
+	/** Create a model of type T in the underlying datastore 
 	 *
-	 * The model is found by the id in the passed object. 
-	 * 
-	 * @param model The updated model to place into the datastore
-	 * @param ec Implicit execution context containing in which to run threads in
-	 * @return A Future containing the result of whether the model was updated or not
+	 * @param model The model which will be created in the datastore
+	 * @param ec Implicit execution context
+	 * @return a Future containing the an object of type T updated with auto-values from the datastore if applicable
 	 */
-	def update(model: T)(implicit ec: ExecutionContext) : Future[Option[T]]
+	 def create(model: T)(implicit ec: ExecutionContext) : Future[T]
 
-	/** Remove a model from the underlying datastore by ither s id
-	 * 
-	 * @param id The id of the model to remove
-	 * @param ec Implicit execution context containing in which to run threads in
-	 * @return A Future containing the result of whether the model was removed or not
+	/** Create a model of type T in the underlying datastore 
+	 *
+	 * @note By not using an id: Long, field we do not tie ourselves to specific numeric ID's for databases, but leave the implementation up to the implementing class of this trait.
+	 *
+	 * @param model The model which will be read from the datastore, with any discrimantory values defined
+	 * @param ec Implicit execution context
+	 * @return a Future containing the an object of type T found by a property on the model
 	 */
-	def remove(id: Long)(implicit ec: ExecutionContext) : Future[Boolean]
+	 def read(model: T)(implicit ec: ExecutionContext) : Future[T]
+
+	/** Update a model of type T in the underlying datastore 
+	 *
+	 * @param model The model which will be updated in the datastore
+	 * @param ec Implicit execution context
+	 * @return a Future containing the an object of type T updated with auto-values from the datastore if applicable
+	 */
+	 def update(model: T)(implicit ec: ExecutionContext) : Future[T]
+
+	/** Delete a model of type T in the underlying datastore 
+	 *
+	 * @note By not using an id: Long, field we do not tie ourselves to specific numeric ID's for databases, but leave the implementation up to the implementing class of this trait.
+	 *
+	 * @note This method is not intended to return a Future[False] value, however this is implementation specific
+	 *
+	 * @param model The model which will be deleted from the datastore, with any discrimantory values defined
+	 * @param ec Implicit execution context
+	 * @return a Future containing the a Boolean value of True if the model was deleted or not found, A failed future otherwise.
+	 */
+	 def delete(model: T)(implicit ec: ExecutionContext) : Future[Boolean]
+
 }
