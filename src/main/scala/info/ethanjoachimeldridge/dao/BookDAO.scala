ba35640f727efef0d1805beb060c2aad5f2c141c
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-08-09 23:33:41 -0400

    Add BookDAO

---
+++ src/main/scala/info/ethanjoachimeldridge/dao/BookDAO.scala
@@ -0,0 +1,16 @@
+package info.ethanjoachimeldridge.dao
+
+import info.ethanjoachimeldridge.model._
+import scala.concurrent.{ExecutionContext, Future}
+
+trait BookDAO extends CrudDAO[Book]{
+
+	/** 
+	 * @param page Which paginated index to use for determining a list result
+	 * @param perPage How many Book's to return in a single List
+	 * @param ec Implicit execution context
+	 * @return A Future containing a List of Book objects
+	 */	
+	def readAll(page: Int, perPage: Int)(implicit ec: ExecutionContext) : Future[List[Book]] = ???
+
+}
