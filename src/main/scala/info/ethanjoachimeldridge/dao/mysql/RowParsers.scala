0a090d1c4d36a94632e2e1e8a11a98a9835d9eb1
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-08-10 00:05:30 -0400

    Add RowParsers
    Since there are multiple services/daos that will handle the data in
    their anorm calls, it's important to put the RowParsers somewhere simple
    where we can get them and not have to repeat them anywhere in the code.

---
+++ src/main/scala/info/ethanjoachimeldridge/dao/mysql/RowParsers.scala
@@ -0,0 +1,40 @@
+package info.ethanjoachimeldridge.dao.mysql
+
+import anorm._
+import anorm.SqlParser._
+
+import java.util.Locale
+
+import info.ethanjoachimeldridge.model.{Author, Book, BookMeta}
+
+object RowParsers {
+
+	/** Simple Anorm Parser to extract Author Records */
+	val authorParser = {
+		get[Long]("author.id") ~ 
+		get[String]("author.name") map {
+			case id ~ name => Author(id, name)
+		}
+	}
+
+	/** Simple Anorm Parser to extract Book Records */
+	val bookParser = {
+		get[Long]("book.authorId") ~
+		get[Long]("book.bookId") map {
+			case authorId ~ bookId => Book(authorId, bookId)
+		}
+	}
+
+	/** Simple Anorm Parser to extract Book Meta Records */
+	val bookMetaParser = {
+		get[Long]("bookMeta.bookId") ~
+		get[String]("bookMeta.lang") ~
+		get[String]("bookMeta.title") ~
+		get[String]("bookMeta.shortDescription") ~
+		get[String]("bookMeta.longDescription") map {
+			case bookId ~ lang ~ title ~ shortDescription ~ longDescription =>
+				BookMeta(bookId, new Locale(lang), title, shortDescription, longDescription)
+		}
+	}
+
+}
