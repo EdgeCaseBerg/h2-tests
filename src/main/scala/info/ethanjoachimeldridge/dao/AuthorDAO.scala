f3b467f809179f99e774a446e4c6f12b53d1db2f
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-08-29 12:29:43 -0400

    Remove ??? from interface
    so that the compiler warns when a method isn't implemented.

--- src/main/scala/info/ethanjoachimeldridge/dao/AuthorDAO.scala
+++ src/main/scala/info/ethanjoachimeldridge/dao/AuthorDAO.scala
@@ -11,12 +11,12 @@ trait AuthorDAO extends CrudDAO[Author]{
 	 * @param ec Implicit execution context
 	 * @return A Future containing a List of Author objects
 	 */	
-	def readAll(page: Int, perPage: Int)(implicit ec: ExecutionContext) : Future[List[Author]] = ???
+	def readAll(page: Int, perPage: Int)(implicit ec: ExecutionContext) : Future[List[Author]]
 
 	/** 
 	 * @page model An author model to look up books for
 	 * @param ec Implicit execution context
 	 * @return A Future containing a List of Book objects and their associated metadata in tuples
 	 */
-	def findBooksByAuthor(model: Author)(implicit ec: ExecutionContext) : Future[List[(Book,List[BookMeta])]] = ???
+	def findBooksByAuthor(model: Author)(implicit ec: ExecutionContext) : Future[List[(Book,List[BookMeta])]]
 }
