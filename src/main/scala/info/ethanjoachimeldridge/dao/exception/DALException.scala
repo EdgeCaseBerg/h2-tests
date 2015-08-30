4ad9e97ea4740a1c9121e6a9026612c8b7ba3850
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-08-30 09:51:34 -0400

    Add DataNotFoundException

--- src/main/scala/info/ethanjoachimeldridge/dao/exception/DALException.scala
+++ src/main/scala/info/ethanjoachimeldridge/dao/exception/DALException.scala
@@ -5,3 +5,5 @@ abstract class DALException extends Exception
 case class DataErrorException(val msg: String, originalException: Throwable) extends DALException
 
 case class DuplicateDataError(val msg: String, originalException: Throwable) extends DALException
+
+case class DataNotFoundException(val msg: String) extends DALException
