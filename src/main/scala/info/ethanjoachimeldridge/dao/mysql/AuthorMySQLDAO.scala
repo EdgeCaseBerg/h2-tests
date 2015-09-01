f633a5cae96a26bb2366c18342d07009fb90a03a
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-09-01 08:00:29 -0400

    Add findBooksByAuthor to AuthorMySQLDAO
    Also update the queries to match the tables defined in the RowParsers.

--- src/main/scala/info/ethanjoachimeldridge/dao/mysql/AuthorMySQLDAO.scala
+++ src/main/scala/info/ethanjoachimeldridge/dao/mysql/AuthorMySQLDAO.scala
@@ -12,7 +12,7 @@ class AuthorMySQLDAO extends AuthorDAO {
 	def create(model: Author)(implicit ec: ExecutionContext): Future[Author] = future {
 		val possibleNewId = MySQLConnector.withTransaction { implicit connection => 
 			val result = SQL(
-				"""INSERT INTO authors (name) VALUES ({name})"""
+				"""INSERT INTO author (name) VALUES ({name})"""
 			).on("name" -> model.name).executeInsert()
 			result
 		}
@@ -24,7 +24,7 @@ class AuthorMySQLDAO extends AuthorDAO {
 	def delete(model: Author)(implicit ec: ExecutionContext): Future[Boolean] = future {
 		MySQLConnector.withTransaction { implicit connection =>
 			val numberEffected = SQL(
-				"""DELETE FROM authors WHERE id = {id}"""
+				"""DELETE FROM author WHERE id = {id}"""
 			).on("id" -> model.id).executeUpdate()
 			true //true because deleting something that doesn't exist is the same result as deleting something that does
 		}
@@ -32,7 +32,7 @@ class AuthorMySQLDAO extends AuthorDAO {
 	def read(model: Author)(implicit ec: ExecutionContext): Future[Author] = future {
 		val readModel : Author = MySQLConnector.withReadOnlyConnection { implicit connection => 
 			val result = SQL(
-				"""SELECT id, name FROM authors WHERE id = {id}"""
+				"""SELECT id, name FROM author WHERE id = {id}"""
 			).on("id" -> model.id).as(RowParsers.authorParser *).headOption
 			val resultModel : Author = result.fold(throw new DataNotFoundException("Author not found"))(identity)
 			resultModel
@@ -43,7 +43,7 @@ class AuthorMySQLDAO extends AuthorDAO {
 	def update(model: Author)(implicit ec: ExecutionContext): Future[Author] = future {
 		val updated = MySQLConnector.withTransaction { implicit connection =>
 			val numberEffected = SQL(
-				"""UPDATE authors SET name = {name} WHERE id = {id}"""
+				"""UPDATE author SET name = {name} WHERE id = {id}"""
 			).on(
 			"name" -> model.name,
 			"id" -> model.id
@@ -60,7 +60,30 @@ class AuthorMySQLDAO extends AuthorDAO {
 		}
 	}
 
-	// def findBooksByAuthor(model: info.ethanjoachimeldridge.model.Author)(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[List[(info.ethanjoachimeldridge.model.Book, List[info.ethanjoachimeldridge.model.BookMeta])]] = ???
-	// def readAll(page: Int,perPage: Int)(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[List[info.ethanjoachimeldridge.model.Author]] = ???
+	def findBooksByAuthor(model: Author)(implicit ec: ExecutionContext): Future[List[(Book, List[BookMeta])]] = future {
+		val books : List[(Book, List[BookMeta])] = MySQLConnector.withReadOnlyConnection { implicit connection =>
+			val combinedParsed = RowParsers.bookParser ~ (RowParsers.bookMetaParser ?)
+			val resultant = SQL(
+				"""
+				SELECT 
+					books.authorId, 
+					books.bookId, 
+					bookMeta.lang, 
+					bookMeta.title, 
+					bookMeta.shortDescription, 
+					bookMeta.longDescription 
+				FROM book LEFT JOIN bookMeta ON book.bookId = bookMeta.bookId
+				WHERE authorId = {authorId}
+				"""
+			).on(
+				"authorId" -> model.id
+			).as(combinedParsed *).groupBy(_._1).map {
+				case (book, l) => (book, l.map(_._2).filter(_.isDefined).map(_.get))
+			}
+			resultant.toList
+		}
+		books
+	}
+	// def readAll(page: Int,perPage: Int)(implicit ec: ExecutionContext): Future[List[Author]] = ???
 
 }
