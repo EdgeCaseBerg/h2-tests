2f10b8cfa3fd0cb54714896a04caaa3cc3f7d4d3
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-09-05 01:57:52 -0400

    Add Unique to name field in migration.
    Remove InnoDB as well so that h2 can load the file. need to figure out
    something for that. maybe an alter table with an ignore?

--- src/main/resources/db/migration/V1__CreateAuthorTable.sql
+++ src/main/resources/db/migration/V1__CreateAuthorTable.sql
@@ -1,4 +1,4 @@
 CREATE TABLE author (
 	id INT(16) UNSIGNED NOT NULL auto_increment PRIMARY KEY,
-	name VARCHAR(64) NOT NULL
-) ENGINE InnoDB COLLATE='utf8_unicode_ci';
+	name VARCHAR(64) NOT NULL UNIQUE
+);
