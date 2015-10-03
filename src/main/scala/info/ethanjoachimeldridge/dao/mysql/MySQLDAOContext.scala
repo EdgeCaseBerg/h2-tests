ee0ea3a8417718a7de8525c73d098b46d3c1b02c
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-10-03 13:33:42 -0400

    Add missing import

--- src/main/scala/info/ethanjoachimeldridge/dao/mysql/MySQLDAOContext.scala
+++ src/main/scala/info/ethanjoachimeldridge/dao/mysql/MySQLDAOContext.scala
@@ -1,5 +1,7 @@
 package info.ethanjoachimeldridge.dao.mysql
 
+import info.ethanjoachimeldridge.dao.{AuthorDAO,BookDAO,DAOContext}
+
 trait MySQLDAOContext extends DAOContext {
 	val authorDAO : AuthorDAO 
 	val bookDAO : BookDAO
