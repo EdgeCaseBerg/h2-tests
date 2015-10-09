9f55f460be0d62cbf33ffb6288867303cb78d692
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-10-09 00:05:18 -0400

    Add method to AuthorService
    and constructor argument, but need to go find a validation library to
    use for validations in the service layer.

--- src/main/scala/info/ethanjoachimeldridge/service/AuthorService.scala
+++ src/main/scala/info/ethanjoachimeldridge/service/AuthorService.scala
@@ -1,7 +1,13 @@
 package info.ethanjoachimeldridge.service
 
 import info.ethanjoachimeldridge.dao.DAOContext
+import info.ethanjoachimeldridge.model._
 
-class AuthorService(implicit daoContext: DAOContext)  {
-	
+import scala.concurrent.{ExecutionContext, Future,future}
+
+class AuthorService(implicit daoContext: DAOContext, ec: ExecutionContext)  {
+	def createAuthor(author: Author) = {
+		//validate
+		daoContext.authorDAO.create(author)
+	}
 }
